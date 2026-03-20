b013d62a00dc935c95b54ef0ec28093c74c552ee
Parwez44 <Kparwez435@gmail.com>
2026-03-21 04:52:29 +0530

    practise qns in class

---
+++ Practise/qn2.java
@@ -0,0 +1,62 @@
+abstract class Device {
+    protected String brand;
+
+    public Device(String brand) {
+        this.brand = brand;
+    }
+
+    public abstract void turnOn();
+}
+
+interface RemoteControllable {
+    void connectToWifi();
+}
+
+interface PowerSaving {
+    String getEnergyRating();
+}
+
+class SmartTV extends Device implements RemoteControllable, PowerSaving {
+
+    public SmartTV(String brand) {
+        super(brand);
+    }
+
+    public void turnOn() {
+        System.out.println(brand + " TV is booting up");
+    }
+
+    public void connectToWifi() {
+        System.out.println("Connecting to Home_5G");
+    }
+
+    public String getEnergyRating() {
+        return "5 Star";
+    }
+}
+
+class ElectricKettle extends Device {
+
+    public ElectricKettle(String brand) {
+        super(brand);
+    }
+
+    public void turnOn() {
+        System.out.println(brand + " Kettle ON");
+    }
+}
+
+public class qn2 {
+    public static void main(String[] args) {
+
+        Device d = new SmartTV("Sony");
+        d.turnOn();
+
+        RemoteControllable r = new SmartTV("LG");
+        r.connectToWifi();
+
+        SmartTV tv = new SmartTV("Samsung");
+        PowerSaving p = tv;
+        System.out.println(p.getEnergyRating());
+    }
+}
